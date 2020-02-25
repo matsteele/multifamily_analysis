9d9e6d96b432cd22a3e901c696e9a4abfdc71558
matsteele <matsteele@gmail.com>
2020-02-24 22:58:45 -0500

    Updates

---
+++ precache-manifest.0194b7bb43aec508efd817c871197ea9.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc281c3615401a9308b9037a262803ba",
+    "url": "/multifamily_analysis/index.html"
+  },
+  {
+    "revision": "591782bc7dc6bb285f1e",
+    "url": "/multifamily_analysis/static/css/main.88493d7a.chunk.css"
+  },
+  {
+    "revision": "9bf2d9ee59603c195675",
+    "url": "/multifamily_analysis/static/js/2.cba897a1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/multifamily_analysis/static/js/2.cba897a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "591782bc7dc6bb285f1e",
+    "url": "/multifamily_analysis/static/js/main.5f716fdc.chunk.js"
+  },
+  {
+    "revision": "51896b8e4ce43a746f4a",
+    "url": "/multifamily_analysis/static/js/runtime-main.743391fd.js"
+  },
+  {
+    "revision": "1bc2434d019541d0b3f5a70368e0069b",
+    "url": "/multifamily_analysis/static/media/KozGoPr6N-ExtraLight.1bc2434d.otf"
+  },
+  {
+    "revision": "f9493375f49656a9860dd9cc285ca4fb",
+    "url": "/multifamily_analysis/static/media/background.f9493375.svg"
+  },
+  {
+    "revision": "fbae44860bf77ea7e2d88330230060a7",
+    "url": "/multifamily_analysis/static/media/housing_bubble-01.fbae4486.svg"
+  },
+  {
+    "revision": "b482304c7752e0cbfb557bca3fd8eea9",
+    "url": "/multifamily_analysis/static/media/housing_bubble-02.b482304c.svg"
+  },
+  {
+    "revision": "cd00237905b2ef569e24151ce0237677",
+    "url": "/multifamily_analysis/static/media/housing_costs-01.cd002379.svg"
+  },
+  {
+    "revision": "a6987eaaf932632b342d82bad44b74c2",
+    "url": "/multifamily_analysis/static/media/housing_costs-02.a6987eaa.svg"
+  },
+  {
+    "revision": "6e94820501f2843371779be12bfedf34",
+    "url": "/multifamily_analysis/static/media/housing_costs-03.6e948205.svg"
+  },
+  {
+    "revision": "d25e7bab2054b9089034f5efe1782807",
+    "url": "/multifamily_analysis/static/media/housing_not_competitive-01.d25e7bab.svg"
+  },
+  {
+    "revision": "310fa9d3caf914ed2c123c99a36c247a",
+    "url": "/multifamily_analysis/static/media/housing_not_competitive-02.310fa9d3.svg"
+  },
+  {
+    "revision": "7750c455d0a61581a6d34ad0968c1ea9",
+    "url": "/multifamily_analysis/static/media/housing_not_competitive-03.7750c455.svg"
+  },
+  {
+    "revision": "d1cca1e898b6454c3ba2098b78ee0869",
+    "url": "/multifamily_analysis/static/media/houstonComparison-01.d1cca1e8.svg"
+  },
+  {
+    "revision": "b69f72114abec1449285df42cadd392d",
+    "url": "/multifamily_analysis/static/media/houstonComparison-02.b69f7211.svg"
+  },
+  {
+    "revision": "d47c015d6155c1fa55701b5788fa690b",
+    "url": "/multifamily_analysis/static/media/houstonComparison-03.d47c015d.svg"
+  },
+  {
+    "revision": "7717f38b48473f09a3ce57ae874ef1da",
+    "url": "/multifamily_analysis/static/media/houstonComparison-pop.7717f38b.svg"
+  },
+  {
+    "revision": "476b3a0d5902340540060dae4d32e1a3",
+    "url": "/multifamily_analysis/static/media/nyc_is_overbuilt-02.476b3a0d.svg"
+  },
+  {
+    "revision": "6c4bd7b1a69e5e4447afd6c47e2e4b8a",
+    "url": "/multifamily_analysis/static/media/prewar_nyc_buildings.6c4bd7b1.png"
+  },
+  {
+    "revision": "54e8a6eac809e86bef9f189f7cd419c9",
+    "url": "/multifamily_analysis/static/media/supply_demand_progression_01.54e8a6ea.svg"
+  },
+  {
+    "revision": "5b144dec9c025442819c4eab7866cc89",
+    "url": "/multifamily_analysis/static/media/supply_demand_progression_02.5b144dec.svg"
+  },
+  {
+    "revision": "27e910a0338d245f8f12e0312a9c3476",
+    "url": "/multifamily_analysis/static/media/supply_demand_progression_03.27e910a0.svg"
+  },
+  {
+    "revision": "b479766c6183ca074f9c710273aedcc4",
+    "url": "/multifamily_analysis/static/media/supply_demand_progression_04.b479766c.svg"
+  },
+  {
+    "revision": "35addab24c8ef31ce45a83e661286d4d",
+    "url": "/multifamily_analysis/static/media/supply_demand_progression_05.35addab2.svg"
+  },
+  {
+    "revision": "eb4976b92b296d38f6779c183e46ac4a",
+    "url": "/multifamily_analysis/static/media/supply_demand_progression_06.eb4976b9.svg"
+  },
+  {
+    "revision": "b1f7c84b5e19e4e3b67d89a4558c90a3",
+    "url": "/multifamily_analysis/static/media/supply_demand_progression_07.b1f7c84b.svg"
+  },
+  {
+    "revision": "6f1888a73ccb94c286b813a02d3c5d6f",
+    "url": "/multifamily_analysis/static/media/whats_not_driving_costs-01.6f1888a7.svg"
+  },
+  {
+    "revision": "9e568fa1ad58cb9f22840e36d28efb00",
+    "url": "/multifamily_analysis/static/media/whats_not_driving_costs-02.9e568fa1.svg"
+  },
+  {
+    "revision": "150f534fa7735d21751a64a21cfbe0fa",
+    "url": "/multifamily_analysis/static/media/whats_not_driving_costs-03.150f534f.svg"
+  },
+  {
+    "revision": "915941729294a18621693729418cd042",
+    "url": "/multifamily_analysis/static/media/whats_not_driving_costs-04.91594172.svg"
+  },
+  {
+    "revision": "ec9e9fe9473489c7408addd8d59f6d10",
+    "url": "/multifamily_analysis/static/media/whats_not_driving_costs-05.ec9e9fe9.svg"
+  },
+  {
+    "revision": "94cdfe492dab8b2e51d85b485dcf2dc5",
+    "url": "/multifamily_analysis/static/media/whats_not_driving_costs-06.94cdfe49.svg"
+  },
+  {
+    "revision": "4e46fca1839760b7b7f6b9a1802c3a9e",
+    "url": "/multifamily_analysis/static/media/whats_not_driving_costs-07.4e46fca1.svg"
+  }
+]);
